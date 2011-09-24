6a8f586c14a9a461b918764db4716a5cd57e3b76
AlleyKat <Chupacabra@live.ru>
2011-09-25 02:08:20 +0400

    25.09.11

--- elements/vars.lua
+++ elements/vars.lua
@@ -15,14 +15,14 @@ if not _G.DerpyNav then
 			["declineduels"] = false,
 			["repair"] = false},
 		
-		["zone_change"] = true,
+		["zone_change"] = true,--
 		["map_show_coord"] = true,
-		["weaponench"] = true,
+		["weaponench"] = true,--
 		["map_show_coord_mouse"] = true,
-		["buffs"] = true,	
-		["debuffs"] = false,
-		["map"] = true,
-		["map_bg"] = true,
+		["buffs"] = true,--	
+		["debuffs"] = false,--
+		["map"] = true,--
+		["map_bg"] = true,--
 		["show_reminder"] = false,
 		
 		["rollbuffs"] = 12,
@@ -36,12 +36,7 @@ if not _G.DerpyNav then
 		["rightmenu"] = {
 			["shown"] = false,
 			["current"] = 1},
-			
-		
-			
-			
-			
-			
+				
 	}
 end
 
@@ -56,3 +51,47 @@ ns[1].territory_colors = {
 	["contested"] = {1.0, 0.7, 0.0},
 	["none"] = {0.1, 1.0, 0.1},
 }
+
+-- Settings
+local C,M,L,V = unpack(select(2,...))
+
+local nav = M.make_settings_template("NAVIGATION",214,340)
+
+M.tweaks_mvn(nav,V,{
+	["map"] = "MAP",
+	["weaponench"] = "ENCHANTS",
+	["zone_change"] = "ZONE UPDATE",
+	["map_bg"] = "BATLLFIELD MAP",
+	["buffs"] = "BUFFS",
+	["debuffs"] = "DEBUFFS",},18)
+
+local st1 = {
+	"rollbuffs",
+	"rolldebuffs",
+	"buffssize",
+	"debuffsize",
+	"bufffont",
+	"debufffont",
+	"map_size_ratio",
+}
+
+local st2 = {
+	"MAX BUFFS PER ROLL",
+	"MAX DEBUFFS PER ROLL",
+	"BUFF ICON SIZE",
+	"DEBUFF ICON SIZE",
+	"BUFF FONT SIZE",
+	"DEBUFF FONT SIZE",}
+	
+local st3 = {32,32,64,64,24,24}
+local pers
+
+for i=1,6 do
+	local a = M.makevarbar(nav,204,0,st3[i],V,st1[i],st2[i])
+		if i == 1 then
+			a:SetPoint("TOP",nav,0,-114)
+		else
+			a:SetPoint("TOP",pers,"BOTTOM")
+		end
+	pers = a
+end
